224ba2dfe63df8fb044af1fee99ece0c1ba4d746
Mahi Reddy <mahidharreddy.work@gmail.com>
2026-04-28 23:56:33 +0530

    render fix 3

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ import teamsRoutes from "./routes/teams";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.use(cors());
 app.use(express.json());
